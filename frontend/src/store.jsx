cd12a749721076b316ed51df682970bca5176fed
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-01 01:41:29 +0530

    chore: Add book slice and genre slice to store

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
+import genreReducer from "./features/genreSlice";
+import bookReducer from "./features/bookSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    genre: genreReducer,
+    book: bookReducer,
   },
 });
 

cf72de812c371dd8377811cbdef1b2f08290a1bc
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-01 01:34:41 +0530

    refactor: Rename getCategories to getGenres for clarity

--- backend/routers/genreRouter.js
+++ backend/routers/genreRouter.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const genreCtrl = require("../controllers/genreCtrl");
 const auth = require("../middlewares/auth");
 
-router.route("/genre").get(auth, genreCtrl.getCategories);
+router.route("/genre").get(auth, genreCtrl.getGenres);
 
 module.exports = router;

bb283dca9ee4e84f12eeffb6fbd98682afe864ec
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-01 01:48:29 +0530

    feat(pages): Create book item as common component

---
+++ frontend/src/components/mainpages/utils/book_item/BookItem.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { FaTrash, FaEye, FaEdit } from "react-icons/fa"; // Import icons
+import { deleteBook } from "../../../../features/bookSlice"; // Import the Redux action
+
+function BookItem({ book }) {
+  // Remove unnecessary props
+  const dispatch = useDispatch();
+
+  const { isLogged, user, accessToken } = useSelector((state) => state.user);
+
+  const { books } = useSelector((state) => state.book); // Get the books array from redux
+
+  // const handleCheck = (id) => {
+  //   const updatedBooks = books.map((p) =>
+  //     p._id === id ? { ...p, checked: !p.checked } : p
+  //   );
+  //   // No need to dispatch an action here if checked is only used for UI state
+  //   // If you need to persist 'checked' state, you can add a redux action to update it.
+  // };
+
+  const handleDelete = () => {
+    // console.log(user);
+    // console.log(accesstoken);
+    if (!user || !isLogged) {
+      return alert("You are not authenticated or token is missing.");
+    }
+    dispatch(deleteBook({ id: book._id, token: accessToken })); // Dispatch Redux delete action
+  };
+
+  // Show delete and edit buttons only if the logged-in user is the author of the book
+  const canEditOrDelete = isLogged && user?._id === book.author?._id;
+
+  return (
+    <div className="book-item bg-white rounded-md shadow-md p-4 flex flex-col gap-2 w-56 mx-auto">
+      <div className="flex justify-end">
+        <Link to={`/detail/${book._id}`} className="btn btn-secondary">
+          <FaEye className="text-blue-500" />
+        </Link>
+      </div>
+      <div className="book-info flex flex-col gap-2">
+        <h2 className="text-lg font-medium truncate text-center">
+          {book.title}
+        </h2>
+        <p className="text-gray-600 text-sm truncate text-center">
+          {book.author ? book.author.name : "Unknown Author"}
+        </p>
+        <p className="text-gray-600 text-sm truncate text-center">
+          {new Date(book.createdAt).toISOString().split("T")[0]}
+        </p>
+      </div>
+      {canEditOrDelete && (
+        <div className="flex justify-center">
+          <Link to={`/edit_book/${book._id}`} className="btn btn-primary">
+            <FaEdit className="text-green-500" />
+          </Link>
+          <button onClick={handleDelete} className="btn btn-danger">
+            <FaTrash className="text-red-500" />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default BookItem;

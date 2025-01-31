9237bf0164964d12f82db1217ebad1f53f1887be
hiru2k <mudannayakehiruni90@gmail.com>
2025-01-31 20:29:15 +0530

    feat(models): Add Book model with references to User and Genre

---
+++ backend/models/bookModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    book_id: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to User model
+      ref: "Users",
+      required: true,
+      immutable: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to Genre model
+      ref: "Genre",
+      required: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Book", bookSchema);

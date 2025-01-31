9abfbe05927b01f0c90717af3c482d86a933b89a
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-01 01:33:56 +0530

    fix: Update error response message for better clarity if token is null

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const auth = (req, res, next) => {
   try {
     const token = req.header("Authorization");
-    if (!token) return res.status(400).json({ msg: "invalid Authorization" });
+    if (!token) return res.status(400).json({ msg: "token is not found" });
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) return res.status(400).json({ msg: "invalid Authorization" });

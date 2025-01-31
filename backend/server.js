dc3f51e4105b716b540712425e57c4394c821b6e
hiru2k <mudannayakehiruni90@gmail.com>
2025-01-31 15:29:27 +0530

    chore(deps): Import dotenv to read env variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const cookieParser = require("cookie-parser");
 
 const app = express();

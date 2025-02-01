94ecf8d5a52b46671c8911926c2b7498edd98b1e
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-01 11:02:19 +0530

    chore: integrate Redux Persist  into App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import MainPages from "./components/mainpages/Pages";
 import Header from "./components/header/Header";
 import { Provider } from "react-redux";
-import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header />
-          <MainPages />
-        </div>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div className="App">
+            <Header />
+            <MainPages />
+          </div>
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }

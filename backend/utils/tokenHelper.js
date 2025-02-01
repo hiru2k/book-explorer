86272a4a562ce2c0cd2427e55fd0026520e2e94c
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-01 21:35:27 +0530

    refactor(auth): Extract token creation from controllers.

---
+++ backend/utils/tokenHelper.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+const createAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
+};
+
+const createRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+};
+
+module.exports = { createAccessToken, createRefreshToken };

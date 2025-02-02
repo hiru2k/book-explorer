cac7b4aa689638ea0c9641e19d9479a4601bf48a
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-02 13:11:52 +0530

    refactor: Extract Bearer token from token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header("Authorization");
+    // const token = req.header("Authorization");
+    const token = req.header("Authorization")?.split(" ")[1]; // Extract token from 'Bearer <token>'
     if (!token) return res.status(400).json({ msg: "token is not found" });
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {

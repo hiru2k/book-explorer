cb9df6f9058b865b7ee5ffc7adc18ed137a1eb88
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-02 13:29:59 +0530

    refactor: Remove unnessary comments to increase redability

--- frontend/src/context/genreContext.jsx
+++ frontend/src/context/genreContext.jsx
@@ -1,30 +1,20 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { useSelector } from "react-redux"; // Import useSelector to access the Redux state
+import { useSelector } from "react-redux";
 import axiosInstance from "../apis/axiosInstance";
 
 const GenreContext = createContext();
-
 export const useGenres = () => useContext(GenreContext);
 
 export const GenreProvider = ({ children }) => {
   const [genres, setGenres] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  // Access token from Redux store
   const { accessToken } = useSelector((state) => state.user);
-  // Retrieve access token from localStorage
-  // const accessToken = localStorage.getItem("accessToken");
 
   useEffect(() => {
     const fetchGenres = async () => {
       setLoading(true);
       try {
-        // Send the access token in the headers
-        const response = await axiosInstance.get("/api/genre", {
-          headers: {
-            Authorization: accessToken,
-          },
-        });
+        const response = await axiosInstance.get("/api/genre");
         setGenres(response.data);
       } catch (error) {
         console.error("Error fetching genres:", error);
@@ -34,10 +24,9 @@ export const GenreProvider = ({ children }) => {
     };
 
     if (accessToken) {
-      // Only call the API if accessToken exists
       fetchGenres();
     }
-  }, [accessToken]); // Fetch genres when accessToken changes
+  }, [accessToken]);
 
   return (
     <GenreContext.Provider value={{ genres, loading }}>

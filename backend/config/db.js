231d60f16cb4dbff06af0f5cf7524283e3a828f1
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-03 04:06:05 +0530

    chore: Set process.env.NODE_ENV to 'test' to prevent DB connection in tests

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,20 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
+  if (process.env.NODE_ENV !== "test") {
+    try {
+      await mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
+    }
+  } else {
+    console.log("Skipping MongoDB connection in test environment.");
   }
 };
 

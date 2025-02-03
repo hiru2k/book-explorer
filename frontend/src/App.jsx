92531cf3c1ae3ead701a954393d7d2dc20069c8c
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-03 06:48:23 +0530

    refactor(imports): Update import statements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/store";
 import { ToastContainer } from "react-toastify";
-import { GenreProvider } from "./context/GenreContext";
+import { GenreProvider } from "./context/genreContext";
 
 function App() {
   return (

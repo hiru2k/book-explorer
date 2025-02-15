d2221ae5186acac4732388ba3f044e0e2ede2a93
hiru2k <mudannayakehiruni90@gmail.com>
2025-02-15 14:44:38 +0530

    feat: Add function to create default genres

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const Genre = require("../models/genreModel");
 
 const connectDB = async () => {
   if (process.env.NODE_ENV !== "test") {
@@ -9,12 +10,29 @@ const connectDB = async () => {
         useUnifiedTopology: true,
       });
       console.log("Connected to MongoDB");
+      await seedGenres();
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
       process.exit(1);
     }
   } else {
     console.log("Skipping MongoDB connection in test environment.");
+  }
+};
+
+// Function to create default genres
+const seedGenres = async () => {
+  const existingGenres = await Genre.countDocuments();
+  if (existingGenres === 0) {
+    const defaultGenres = [
+      { name: "Fiction" },
+      { name: "Non-Fiction" },
+      { name: "Science" },
+      { name: "History" },
+    ];
+
+    await Genre.insertMany(defaultGenres);
+    console.log("Default genres added.");
   }
 };
 
